ec606c2031d81275899d28b9c89915de7736d7a2
Tim Fulmer <me@timfulmer.com>
2016-08-14 22:50:32 -0700

    Updated for working sample with fake DNS

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var couchbase = require('couchbase');
-//var cluster = new couchbase.Cluster('couchbase://23.23.64.181:11211?detailed_errcodes=1');
-var cluster = new couchbase.Cluster('http://23.23.64.181:8091?detailed_errcodes=1');
-var bucket = cluster.openBucket('grocery-sync');
+var cluster = new couchbase.Cluster('couchbase.host');
+var bucket = cluster.openBucket('beer-sample');
+// var bucket = cluster.openBucket('grocery-sync');
 bucket.upsert('testdoc', {name:'Frank'}, function(err, result) {
   if (err) throw err;
 
@@ -9,6 +9,8 @@ bucket.upsert('testdoc', {name:'Frank'}, function(err, result) {
     if (err) throw err;
 
     console.log(result.value);
-    // {name: Frank}
+    // => {name: Frank}
+    // success!!
+    process.exit(0);
   });
 });
